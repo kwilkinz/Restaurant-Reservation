8643c81ad912e1310f9fe7d9ebaa2524ba8a7819
Keri Wilkins <keriannwilkins@gmail.com>
2022-12-06 15:01:24 -0600

    added db migrations and updated seed (#5)

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,8 +1,9 @@
 const service = require("./reservation.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
-/**
- * List handler for reservation resources
- */
+
+// from utils api 
+const dateFormatted = /[0-9]{4}-[0-9]{2}-[0-9]{2}/;
+const timeFormatted = /[0-9]{2}:[0-9]{2}/;
 
 async function list(req, res) {
   res.json({
@@ -10,6 +11,11 @@ async function list(req, res) {
   });
 }
 
+// Create 
+
+// middleware (valid properties, booked, valid day)
+
+// 
 module.exports = {
   list,
   create: asyncErrorBoundary(create),
--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -4,12 +4,22 @@ const methodNotAllowed = require("../errors/methodNotAllowed")
 
 /**
  * Defines the router for reservation resources.
- * URL = "/"
  */
 
-// - return list of all movies
+// - US-02 - return list of all movies
 router.route("/")
     .get(controller.list)
+    .all(methodNotAllowed);
+
+// - retrives reservation id 
+router.route("/:reservation_id")
+    // .get()
+    // .put()
+    .all(methodNotAllowed);
+
+// - US-06 - displays status of the reservation
+router.route("/:reservation_id/status")
+    // .put()
     .all(methodNotAllowed);
 
 module.exports = router;
