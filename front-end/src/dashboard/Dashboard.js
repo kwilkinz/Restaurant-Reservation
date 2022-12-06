b23e84524abc39c298fc85be13a00b84c5fa0af8
Keri Wilkins <keriannwilkins@gmail.com>
2022-12-06 11:41:03 -0600

    basic button setup and form complete for new reservation (#3)

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { listReservations } from "../utils/api";
 import ErrorAlert from "../layout/errors/ErrorAlert";
+import viewAllReservation from "./ViewReservations";
 
 /**
  * Defines the dashboard page.
@@ -8,12 +9,19 @@ import ErrorAlert from "../layout/errors/ErrorAlert";
  *  the date for which the user wants to view reservations.
  * @returns {JSX.Element}
  */
+
 function Dashboard({ date }) {
+
   const [reservations, setReservations] = useState([]);
   const [reservationsError, setReservationsError] = useState(null);
 
   useEffect(loadDashboard, [date]);
 
+  //
+  
+
+
+  // calls the listReservations from utils.api
   function loadDashboard() {
     const abortController = new AbortController();
     setReservationsError(null);
@@ -23,12 +31,25 @@ function Dashboard({ date }) {
     return () => abortController.abort();
   }
 
+  // loop through the list to print all the reservations 
+  const reservationList = reservations.map((reservation, index) => {
+    if (reservation.status === "finished" || reservation.status === "canceled") return null;
+    <viewAllReservation key={index} reservation={reservation} />   
+  })
+
   return (
     <main>
       <h1>Dashboard</h1>
       <div className="d-md-flex mb-3">
         <h4 className="mb-0">Reservations for date</h4>
+        <button className="btn btn-info px-3 py-2"> Previous </button>
+        <button className="btn btn-info px-3 py-2"> Next </button>
+        <button className="btn btn-info px-3 py-2"> Today </button>
       </div>
+      <div>
+        <div className="container fluid">{reservationList}</div>
+      </div>
+
       <ErrorAlert error={reservationsError} />
       {JSON.stringify(reservations)}
     </main>
--- front-end/src/layout/components/reservation/NewReservation.js
+++ front-end/src/layout/components/reservation/NewReservation.js
@@ -1,22 +1,109 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 function NewReservation() {
+
+  const [newRes, setNewRes] = useState();
+  const history = useHistory();
+
+
+  // handleChange for text
+  const handleChange = ({ target }) => {
+    setNewRes({
+      ...newRes,
+      [target.name]: target.value,
+    });
+  };
+
+  //after clicked directed to the dashboard
+  async function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
     <div>
-      <h1>Connected to Form</h1>
-      <form >
-      {/* <form name="newReservation" onSubmit={handleSubmit}> */}
-        <label htmlFor="first_name"> First Name: 
-        <input 
-            id="first_name"
-            type="text"
-            name="first_name"
-            // onChange={handleChange}
-            
-        
-        /> 
-        </label>
+      <h1>Make a New Reservation</h1>
+      <form className="d-flex flex-column" onSubmit={handleSubmit}>
+        {/* First Name */}
+        <label htmlFor="first_name"> First Name: </label>
+        <input
+          className="form-control my-1"
+          id="first_name"
+          type="text"
+          name="first_name"
+          onChange={handleChange}
+          // value={newRes.name}
+          placeholder="Jane"
+        />
 
+        {/* Last Name */}
+        <label htmlFor="last_name"> Last Name: </label>
+        <input
+          className="form-control my-1"
+          id="last_name"
+          type="text"
+          name="last_name"
+          onChange={handleChange}
+          // value={newRes.name}
+          placeholder="Doe"
+        />
+
+        {/* Phone Number */}
+        <label htmlFor="mobile_number"> Phone Number: </label>
+        <input
+          className="form-control my-1"
+          id="mobile_number"
+          type="tel"
+          name="mobile_number"
+          onChange={handleChange}
+          //value={form[mobile_number]}
+          placeholder="(---) --- ----"
+        />
+
+        {/* Reservation Date */}
+        <label htmlFor="reservation_date"> Reservation Date: </label>
+        <input
+          className="form-control my-1"
+          id="reservation_date"
+          type="date"
+          name="reservation_date"
+          onChange={handleChange}
+          //value={form[reservation_date]}
+          pattern="\d{4}-\d{2}-\d{2}"
+          placeholder="YYYY-MM-DD"
+        />
+
+         {/* Time Reservation */}
+         <label htmlFor="reservation_time"> Reservation Time: </label>
+        <input
+          className="form-control my-1"
+          id="reservation_time"
+          type="time"
+          name="reservation_time"
+          onChange={handleChange}
+          //value={form[reservation_time]}
+          pattern="[0-9]{2}:[0-9]{2}"
+          placeholder="HH:MM"
+        />
+
+         {/* People in Reservation */}
+         <label htmlFor="people"> People In Party: </label>
+        <input
+          className="form-control my-1"
+          id="people"
+          type="number"
+          name="people"
+          onChange={handleChange}
+          //value={form[people]}
+          min={1}
+          placeholder="2"
+        />
+
+        {/* Submit Button */}
+        <button className="btn btn-primary"> Submit </button>
+
+        {/* Cancel Button goes back previous page */}
+        <button className="btn btn-secondary" onClick={() => history.goBack()}> Cancel </button>
       </form>
     </div>
   );
